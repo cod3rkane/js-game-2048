fa7de28434978bf20646672d041dca72f4795730
cod3rkane <jcesar.developer@gmail.com>
2018-03-19 22:55:00 -0300

    webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,59 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, "dist"),
+  SRC: path.resolve(__dirname, "src")
+};
 
 module.exports = {
-  entry: './src/index.js',
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  },
+  entry: ["babel-polyfill", path.join(paths.SRC, "index.js")],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: paths.DIST
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, "index.html")
+    })
+  ],
+  devServer: {
+    contentBase: paths.SRC,
+    historyApiFallback: true,
+    port: 3030
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
   }
 };
