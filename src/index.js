6b865bd85b4dd0d24d88178f4184649c59ed92d2
cod3rkane <jcesar.developer@gmail.com>
2018-03-22 00:19:44 -0300

    Adding Matrix Up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Grid from './grid';
+import Matrix from './matrix';
 
 const canvas = document.getElementById('app');
 const context = canvas.getContext('2d');
 
 const grid = new Grid({ context, width: 500, height: 500 });
 grid.draw();
+
+const matrix = new Matrix();
+matrix.up();
+console.table(matrix.matrix);
--- src/matrix.js
+++ src/matrix.js
@@ -1,10 +1,36 @@
+import { MAX_COLS } from './grid';
+
 export default class Matrix {
   constructor() {
     this.matrix = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
+      [3, 0, 0, 0],
+      [0, 4, 2, 0],
+      [0, 0, 4, 2],
+      [2, 2, 0, 2],
     ];
+  }
+
+  up() {
+    for (let row = 0; row < MAX_COLS; row += 1) {
+      for (let col = 0; col < MAX_COLS; col += 1) {
+        const value = this.matrix[row][col];
+        if (value !== 0 && row !== 0) {
+          let valueRow = row;
+          this.matrix[row][col] = 0;
+
+          do {
+            if (this.matrix[valueRow - 1][col] === 0) {
+              valueRow -= 1;
+            } else {
+              break;
+            }
+          } while (valueRow > 0 && valueRow < MAX_COLS);
+
+          if (this.matrix[valueRow][col] === 0) {
+            this.matrix[valueRow][col] = value;
+          }
+        }
+      }
+    }
   }
 }
